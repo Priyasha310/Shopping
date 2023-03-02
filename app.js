9f495b705d9a1944c05bff9802d2b34d6ea6970e
Priyasha310 <72645022+Priyasha310@users.noreply.github.com>
2023-03-02 14:12:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://127.0.0.1:27017/custDB", {useNewUrlParser: true});
 
 const custSchema = new mongoose.Schema({
     email: String, 
